7b123d3ab738c60b6fa04844d062aef8b5bd2ec1
david <davidrechdiner@gmail.com>
2025-09-01 09:52:37 +0300

    update proj

--- keylogger-agent/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    UPDATE_INTERVAL = 5
-    ENCRYPTION_KEY = "42"
-    SERVER_URL = "http://127.0.0.1:5000/api/upload"
-    MACHINE_NAME = "TestMachine"
-    LOG_DIRECTORY = "./logs"
--- keylogger-agent/interfaces/iwriter.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-# פונקציה אבסטרקטית לשליחת המידע לשרת
-class IWriter(ABC):
-    @abstractmethod
-    def send_data(self, data: str, machine_name: str) -> None:
-        pass
-
